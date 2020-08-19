03760231a4f4967aa2a30cb70db8bdafebb8904f
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2020-08-18 22:51:52 -0600

    THE NAMELESS ONE WORKS!

--- src/main/java/com/gizmo/unusedassets/entity/dungeons/NamelessEntity.java
+++ src/main/java/com/gizmo/unusedassets/entity/dungeons/NamelessEntity.java
@@ -1,11 +1,12 @@
 package com.gizmo.unusedassets.entity.dungeons;
 
+import net.minecraft.entity.CreatureEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
-import net.minecraft.entity.monster.MonsterEntity;
+import net.minecraft.entity.ai.goal.LookRandomlyGoal;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import software.bernie.geckolib.animation.builder.AnimationBuilder;
@@ -15,44 +16,47 @@ import software.bernie.geckolib.entity.IAnimatedEntity;
 import software.bernie.geckolib.event.AnimationTestEvent;
 import software.bernie.geckolib.manager.EntityAnimationManager;
 
-public class NamelessEntity extends MonsterEntity implements IAnimatedEntity {
+public class NamelessEntity extends CreatureEntity implements IAnimatedEntity {
 
 	EntityAnimationManager manager = new EntityAnimationManager();
-	AnimationController<NamelessEntity> idle = new EntityAnimationController<NamelessEntity>(this, "moveController", 10, this::moveController);
-	
+	AnimationController<NamelessEntity> idle = new EntityAnimationController<NamelessEntity>(this, "moveController", 10,
+			this::moveController);
+
 	private <E extends Entity> boolean moveController(AnimationTestEvent<E> event) {
-		if(event.isWalking() == false) {
-			idle.setAnimation(new AnimationBuilder().addAnimation("animation.nameless.idlestart").addAnimation("animation.nameless.idleloop", true));
+		if (event.isWalking() == false) {
+			idle.setAnimation(new AnimationBuilder().addAnimation("animation.nameless.idlestart")
+					.addAnimation("animation.nameless.idleloop", true));
 		} else {
 			idle.setAnimation(new AnimationBuilder().addAnimation("animation.nameless.summon"));
-		}	
-		return false;
+		}
+		return true;
 	}
-	
-	
-	public ResourceLocation getTexture() {
-	    return new ResourceLocation("unusedassets", "textures/entity/dungeons/nameless_king.png");
-	  }
-	
-	public NamelessEntity(EntityType<? extends MonsterEntity> entityTypeIn, World worldIn) {
+
+	public NamelessEntity(EntityType<? extends CreatureEntity> entityTypeIn, World worldIn) {
 		super(entityTypeIn, worldIn);
 		registerControllers();
 		manager.addAnimationController(idle);
 	}
-	
-	public static AttributeModifierMap.MutableAttribute attributes() {
-	      return MobEntity.func_233666_p_().createMutableAttribute(Attributes.MAX_HEALTH, 300.0D);
+
+	public void registerGoals() {
+		this.goalSelector.addGoal(0, new LookRandomlyGoal(this));
 	}
-	
+
+	public ResourceLocation getTexture() {
+		return new ResourceLocation("unusedassets", "textures/entity/dungeons/nameless_king.png");
+	}
+
+	public static AttributeModifierMap.MutableAttribute attributes() {
+		return MobEntity.func_233666_p_().createMutableAttribute(Attributes.MAX_HEALTH, 300.0D);
+	}
+
 	@Override
 	public EntityAnimationManager getAnimationManager() {
 		return manager;
 	}
-	
+
 	public void registerControllers() {
 		manager.addAnimationController(idle);
 	}
-	
-	
 
 }
