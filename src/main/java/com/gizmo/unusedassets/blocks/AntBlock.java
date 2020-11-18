f54534d8c074cd350b042bef917ce7ce74fcba81
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2020-11-18 15:24:00 -0700

    Tried to fix the Ant Block

--- src/main/java/com/gizmo/unusedassets/blocks/AntBlock.java
+++ src/main/java/com/gizmo/unusedassets/blocks/AntBlock.java
@@ -1,6 +1,7 @@
 package com.gizmo.unusedassets.blocks;
 
 import java.util.Random;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
@@ -9,8 +10,8 @@ import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.state.DirectionProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.util.Direction;
-import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 
@@ -23,55 +24,55 @@ public class AntBlock extends Block {
 		setDefaultState((this.stateContainer.getBaseState()).with(FACING, Direction.NORTH));
 	}
 
-	enum Step {
-		CW, CCW;
-	}
-
-	public void tick(BlockState state, ServerWorld worldIn, BlockPos pos, Random random) {
-		BlockState blockState = worldIn.getBlockState(pos.down());
+	@Override
+	public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+		BlockState blockState = world.getBlockState(pos.down());
 		if (blockState.getBlock() == Blocks.WHITE_CONCRETE) {
-			move(state, worldIn, pos, Step.CW);
+			this.move(state, world, pos, AntBlock.Clockwiseness.CW);
 		} else if (blockState.getBlock() == Blocks.BLACK_CONCRETE) {
-			move(state, worldIn, pos, Step.CCW);
+			this.move(state, world, pos, AntBlock.Clockwiseness.CCW);
 		}
+
 	}
 
-	private void move(BlockState state, ServerWorld worldIn, BlockPos pos, Step step) {
-		Direction direction1 = state.get(FACING);
-		Direction direction2 = (step == Step.CW) ? direction1.rotateY() : direction1.rotateYCCW();
-		BlockPos pos1 = pos.offset(direction2);
-		if (worldIn.isBlockLoaded(pos1))
-			switch (step) {
+	private void move(BlockState state, ServerWorld world, BlockPos pos, AntBlock.Clockwiseness clockwiseness) {
+		Direction direction = state.get(FACING);
+		Direction direction2 = clockwiseness == AntBlock.Clockwiseness.CW ? direction.rotateY()
+				: direction.rotateYCCW();
+		BlockPos blockPos = pos.offset(direction2);
+		if (world.isBlockLoaded(blockPos)) {
+			switch (clockwiseness) {
 			case CW:
-				worldIn.setBlockState(pos.down(), Blocks.BLACK_CONCRETE.getDefaultState(), 19);
-				worldIn.setBlockState(pos, Blocks.AIR.getDefaultState(), 3);
-				worldIn.setBlockState(pos1, state.with(FACING, direction2), 3);
+				world.setBlockState(pos.down(), Blocks.BLACK_CONCRETE.getDefaultState(), 19);
+				world.setBlockState(pos, Blocks.AIR.getDefaultState(), 3);
+				world.setBlockState(blockPos, (BlockState) state.with(FACING, direction2), 3);
 				break;
 			case CCW:
-				worldIn.setBlockState(pos.down(), Blocks.WHITE_CONCRETE.getDefaultState(), 19);
-				worldIn.setBlockState(pos, Blocks.AIR.getDefaultState(), 3);
-				worldIn.setBlockState(pos1, state.with(FACING, direction2), 3);
-				break;
+				world.setBlockState(pos.down(), Blocks.WHITE_CONCRETE.getDefaultState(), 19);
+				world.setBlockState(pos, Blocks.AIR.getDefaultState(), 3);
+				world.setBlockState(blockPos, (BlockState) state.with(FACING, direction2), 3);
 			}
+		}
+
 	}
-
-//	  public BlockState updateShape(BlockState state, Direction direction, BlockState blockState1, LevelAccessor levelAccessor, BlockPos pos1, BlockPos pos2) {
-//	    return super.updateShape(state, direction, blockState1, levelAccessor, pos1, pos2);
-//	  }
-
-	public void onPlace(BlockState state, World level, BlockPos blockPos, BlockState blockState1, boolean bool) {
-		level.getPendingBlockTicks().scheduleTick(blockPos, this, 1);
-	}
-
+	
 	public BlockState getStateForPlacement(BlockItemUseContext context) {
 		return getDefaultState().with(FACING, context.getPlacementHorizontalFacing().getOpposite());
+	}
+
+	public BlockState updatePostPlacement(BlockState state, Direction direction, BlockState newState, IWorld world, BlockPos pos, BlockPos posFrom) {
+		return super.updatePostPlacement(state, direction, newState, world, pos, posFrom);
+	}
+
+	public void onBlockAdded(BlockState state, World world, BlockPos pos, BlockState oldState, boolean notify) {
+		world.getPendingBlockTicks().scheduleTick(pos, this, 1);
 	}
 
 	protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
 		builder.add(FACING);
 	}
 
-	public BlockState rotate(BlockState state, Rotation rot) {
-		return state.with(FACING, rot.rotate((Direction) state.get(FACING)));
+	static enum Clockwiseness {
+		CW, CCW;
 	}
 }
